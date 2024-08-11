0c1c5cacfe25ddde3ace90e0170c1d9ad090ea5a
Anand <mishraanand3537@gmail.com>
2024-08-11 19:34:57 +0530

    Multiplying two numbers

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+package java_coding;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int num1 = 23;
+        int num2 = 12;
+        int mul = num1*num2;
+        System.out.println(mul);
+    }
+}
