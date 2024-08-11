b018f80c63b080f49f10dc86588e981285454369
Anand <mishraanand3537@gmail.com>
2024-08-11 19:27:36 +0530

    I write the two program of adding and subtraction of two numbers

---
+++ Subtract.java
@@ -0,0 +1,10 @@
+package java_coding;
+
+public class Subtract {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 13;
+        int diff = num1 - num2;
+        System.out.println("The difference between two numers is : "+ diff);
+    }
+}
